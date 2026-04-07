38da830b84f00b5d95867e5d33074589484ed705
John Lian R. Nerecina <johnliannerecina@gmail.com>
2026-04-08 07:35:03 +0800

    Update database connection settings for hosting (infinityfree)

--- config.php
+++ config.php
@@ -1,5 +1,19 @@
 <?php
-$host = 'localhost';
+//For infinityfree hosting
+$host = 'sql213.infinityfree.com';
+$dbname = 'if0_41547962_book_info';
+$username = 'if0_41547962';
+$password = 'Nrur129l1I';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+
+//For localhosting
+/*$host = 'localhost';
 $dbname = 'books_info';
 $username = 'root';
 $password = '';
@@ -9,5 +23,5 @@ try {
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
-}
+}*/
 ?>
